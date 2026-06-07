cbf97238a8e13439430b230bb12b9011e4847bc6
hts <hts@local>
2026-06-07 22:29:16 +0530

    Update Coinvera wordmark styling

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -12,11 +12,11 @@ export function Brand({ dark = false }: BrandProps) {
         <path className="brandRupee" d="M20 28h30M20 38h28M49 28c0 20-14 31-30 31l23 25M22 48h28" />
         <path className="brandDivider" d="M46 20v56" />
         <path className="brandT" d="M53 28h28M67 28v43M55 53c8-6 24-6 32 0M57 61c8-5 20-5 28 0" />
-      </svg>
+        </svg>
       <span className="brandText">
         <span className="brandName">
-          <span>Coin</span>
-          <span>vera</span>
+          <span>COIN</span>
+          <span>VERA</span>
         </span>
         <span className="brandTagline">
           <Sparkles size={12} />
